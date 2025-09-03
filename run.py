6567984f2ab0fe3a728a2a28880571f8b972f743
Darkcoder-paditya <prashant.aditya.civ20@itbhu.ac.in>
2025-09-04 00:43:21 +0530

    Some personal changes

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ from app import create_app
 app = create_app(os.getenv("ENV") or "dev")
 
 if __name__ == '__main__':
+    # the changes are mine, infact the whole project is mine
     app.run(host="0.0.0.0", port=5000, debug = True)
